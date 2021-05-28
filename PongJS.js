9696ee1b576972bdc8d0b8cc252555b8c972a4b0
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2021-05-28 22:32:41 +0900

    Update PongJS.js

--- PongJS.js
+++ PongJS.js
@@ -3,7 +3,7 @@
 // 0-based (map) indexing
 
 const block_colors = ["#000000", "#111111"]
-change_speed(1);
+change_speed(-1);
 
 // FROM HERE
 /**
@@ -47,11 +47,11 @@ function hsvToRgb(h, s, v) {
 
 function domove(x, y, c, v) {
 	var s = round((1 - v) * 5);
-	change_speed(10);
+	change_speed(49);
 	for(i = 0; i < s; i++)
 		for(q = 0; q < 4; q++)
 			turn_left(); // 시간 지연용 제자리 돌기
-	change_speed(1);
+	change_speed(-1);
 	var color = hsvToRgb((((c * 3) % 360) / 360.0), 0.7 - (1.0 - v) * 0.7, 0.9 + (1.0 - v) * 0.1);
 	var r = (round(color[0])).toString(16);
 	var g = (round(color[1])).toString(16);
@@ -62,7 +62,7 @@ function domove(x, y, c, v) {
 	if(!block_colors.includes(get_color(get_y(), get_x()))) set_color(get_y(), get_x(), "#" + r + g + b);
 	//print("Vel  " + s + " v " + v);
 	space_jump(y, x);
-	//clear_move(); // 리소스 엄청 
+	//clear_move(); // 리소스 엄청 먹더라
 }
 
 function set_direction(d) {
@@ -106,7 +106,7 @@ class racket {
 		}
 		this.moving = false;
 		//setTimeout(callback);
-		//print("RkIc " + this.y);
+		print("RkIc " + this.y);
 	}
 	decr() {
 		if(this.moving) return;
@@ -119,7 +119,7 @@ class racket {
 		}
 		this.moving = false;
 		//setTimeout(callback);
-		//print("RkDc " + this.y);
+		print("RkDc " + this.y);
 	}
 }
 
@@ -134,7 +134,7 @@ class pong {
 		//this.contdrag = -0.005;
 		this.contdrag = 0.0;
 		this.init();
-		//this.loopid = undefined;
+		this.loopid = null;
 	}
 	init() {
 		var initangle = Math.random() * 2 * Math.PI;
@@ -149,60 +149,60 @@ class pong {
 	}
 	roll() {
 		if(this.c != 0 && this.brk == true) this.init();
-		while(this.rep()) {}
-	}
-	rep() {
-		var i = this.step();
-		this.c += 1;
-		if(i > 0) {
-			return false;
-		} else return true;
+		var setInterval = window.setInterval;
+		this.loopid = setInterval(pong2["pong1"].step, 100);
 	}
 	step() {
-		var tx = this.ball["p"][0] + this.ball["v"][0];
-		var ty = this.ball["p"][1] + this.ball["v"][1];
-		var vt = Math.sqrt(this.ball["v"][0]*this.ball["v"][0] + this.ball["v"][1]*this.ball["v"][1]);
+		var me = pong2["pong1"];
+		me.c += 1;
+		var tx = me.ball["p"][0] + me.ball["v"][0];
+		var ty = me.ball["p"][1] + me.ball["v"][1];
+		var vt = Math.sqrt(me.ball["v"][0]*me.ball["v"][0] + me.ball["v"][1]*me.ball["v"][1]);
 		var crashdir = 5;
 		if(tx > get_max_x()) {
 			tx = 2.0 * get_max_x() - tx;
-			this.ball["v"][0] *= -1;
+			me.ball["v"][0] *= -1;
 			crashdir += +1;
 		} else if(tx < 0.0) {
 			tx = -tx;
-			this.ball["v"][0] *= -1;
+			me.ball["v"][0] *= -1;
 			crashdir += -1;
 		}
 		if(ty > get_max_y()) {
 			ty = 2.0 * get_max_y() - ty;
-			this.ball["v"][1] *= -1;
+			me.ball["v"][1] *= -1;
 			crashdir += -3;
 		} else if(ty < 0.0) {
 			ty = -ty;
-			this.ball["v"][1] *= -1;
+			me.ball["v"][1] *= -1;
 			crashdir += +3;
 		}
 		if(crashdir != 5) {
-			if(this.crashwall(this.ball, crashdir) > 0) return 3;
+			if(me.crashwall(me.ball, crashdir) > 0) {
+				clearInterval(me.loopid);
+				me.loopid = null;
+				return 3;
+			}
 		}
 
 		if(block_colors.includes(get_color(round(ty), round(tx)))) { // get_color 함수 인자의 X와 Y가 바뀌어 있더이다
 			var crashdir = 5
-			if(this.ball["v"][0] < 0.0 && block_colors.includes(get_color(get_y(), get_x() - 1))) {
+			if(me.ball["v"][0] < 0.0 && block_colors.includes(get_color(get_y(), get_x() - 1))) {
 				tx = 2.0 * get_x() - tx;
-				this.ball["v"][0] *= -1;
+				me.ball["v"][0] *= -1;
 				crashdir += -1;
-			} else if(this.ball["v"][0] > 0.0 && block_colors.includes(get_color(get_y(), get_x() + 1))) {
+			} else if(me.ball["v"][0] > 0.0 && block_colors.includes(get_color(get_y(), get_x() + 1))) {
 				tx = 2.0 * get_x() - tx;
-				this.ball["v"][0] *= -1;
+				me.ball["v"][0] *= -1;
 				crashdir += +1;
 			}
-			if(this.ball["v"][1] < 0.0 && block_colors.includes(get_color(get_y() - 1, get_x()))) {
+			if(me.ball["v"][1] < 0.0 && block_colors.includes(get_color(get_y() - 1, get_x()))) {
 				ty = 2.0 * get_y() - ty;
-				this.ball["v"][1] *= -1;
+				me.ball["v"][1] *= -1;
 				crashdir += +3;
-			} else if(this.ball["v"][1] > 0.0 && block_colors.includes(get_color(get_y() + 1, get_x()))) {
+			} else if(me.ball["v"][1] > 0.0 && block_colors.includes(get_color(get_y() + 1, get_x()))) {
 				ty = 2.0 * get_y() - ty;
-				this.ball["v"][1] *= -1;
+				me.ball["v"][1] *= -1;
 				crashdir += -3;
 			}
 			if(crashdir == 5) {
@@ -214,21 +214,27 @@ class pong {
 					else if(round(ty) < get_y()) crashdir = 7;
 				}
 			}
-			if(this.crashblock(this.ball, crashdir) > 0) return 2;
+			if(me.crashblock(me.ball, crashdir) > 0) {
+				clearInterval(me.loopid);
+				me.loopid = null;
+				return 2;
+			}
 		}
 
-		this.ball["p"][0] = tx;
-		this.ball["p"][1] = ty;
+		me.ball["p"][0] = tx;
+		me.ball["p"][1] = ty;
 
-		domove(round(tx), round(ty), this.c, vt);
+		domove(round(tx), round(ty), me.c, vt);
 
 		if(vt <= 0.0) {
-			print("Stop " + this.ball["p"]);
+			print("Stop " + me.ball["p"]);
+			clearInterval(me.loopid);
+			me.loopid = null;
 			return 1;
 		}
 
-		this.ball["v"][0] *= Math.max((vt + this.contdrag) / vt, 0.0);
-		this.ball["v"][1] *= Math.max((vt + this.contdrag) / vt, 0.0); // 속도에 상관없이 일정한 감속을 부여: 수정 필요함
+		me.ball["v"][0] *= Math.max((vt + me.contdrag) / vt, 0.0);
+		me.ball["v"][1] *= Math.max((vt + me.contdrag) / vt, 0.0); // 속도에 상관없이 일정한 감속을 부여: 수정 필요함
 		return -1;
 	}
 }
@@ -236,11 +242,10 @@ class pong {
 var pong1 = new pong(crashwall, crashblock);
 var pong2 = {"pong1": pong1}
 
-press_key("space", "pong2[\"pong1\"].roll()");
+//press_key("space", "pong2[\"pong1\"].roll()");
 press_key("w", "rk1.incr()");
 press_key("s", "rk1.decr()");
 press_key("i", "rk2.incr()");
 press_key("k", "rk2.decr()");
-//button("buttonstart", "시작", "pong2[\"pong1\"].roll()");
-
-print("PRESS SPACE TO CONTINUE");
+pong2["pong1"].roll();
+//print("PRESS SPACE TO CONTINUE");
