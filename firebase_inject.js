80625a43df8678fde604822b468bc089e15fbeda
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2021-05-29 17:42:54 +0900

    Create firebase_inject.js

---
+++ firebase_inject.js
@@ -0,0 +1,46 @@
+if(!($("#dscriptarea").length)) {
+  S("#executearea").before("<div id=\"dscriptarea\"></div>")
+  $("#dscriptarea").append("<script src=\"https://www.gstatic.com/firebasejs/8.5.0/firebase.js\"></script>")
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyA57g8zdxp0tbpFiSKdOy17jIrWRoWXc1c",
+    authDomain: "pongjs-playbot.firebaseapp.com",
+    projectId: "pongjs-playbot",
+    storageBucket: "pongjs-playbot.appspot.com",
+    messagingSenderId: "40502297964",
+    appId: "1:40502297964:web:0cf67089337b1ed15d5f49",
+    measurementId: "G-NCNKC7D3T7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  var db = firebase.firestore();
+  var ldb = db.collection("leaderboard");
+}
+
+function addleaderboard(score) {
+  ldb.set({
+      id: _userid,
+      name: _username,
+      at: Timestamp.now(),
+      score: score
+  }).then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+  }).catch((error) => {
+      console.error("Error adding document: ", error);
+  });
+}
+
+function getleaderboard() {
+  ldb.orderBy("score", "desc").limit(5).get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id + " => " + doc.data());
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: " + error);
+    });
+}
