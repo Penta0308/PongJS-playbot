1d2feed41bbb9e310049da63aee6104872fbd6c9
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2021-05-30 23:19:27 +0900

    Update PongJS.js

--- PongJS.js
+++ PongJS.js
@@ -14,15 +14,16 @@ const layer_ball = 0;
 const layer_racket = 1;
 const ballspeed = 70;
 var racketl = 4;
-const learningRate = 0.00001;
+const learningRate = 0.001;
 
 class ai {
 	constructor() {
 		print("Starting Ai...");
 		this.model = tf.sequential();
-		this.model.add(tf.layers.dense({units: 64, inputShape: [8]})); //input is a 1x8
-		this.model.add(tf.layers.dense({units: 64, activation: 'relu6'}));
-		this.model.add(tf.layers.dense({units: 3, activation: 'relu6'})); //returns a 1x3
+		this.model.add(tf.layers.dense({units: 64, inputShape: [4]}));
+		this.model.add(tf.layers.dense({units: 128, activation: 'relu6'}));
+		this.model.add(tf.layers.dense({units: 128, activation: 'relu6'}));
+		this.model.add(tf.layers.dense({units: get_max_y(), activation: 'relu6'}));
 		this.optimizer = tf.train.adam(learningRate);
 		this.model.compile({loss: 'meanSquaredError', optimizer: this.optimizer});
 		this.previous_data = null;
@@ -38,8 +39,9 @@ class ai {
 		//this.training_data[1][this.training_data[1].length - 1] = [-1.0];
 		this.train();
 	}
-	save_data(humany, computery, ballx, bally) {
-		var data_xs = [humany, computery, get_max_x() - ballx, bally];
+	save_data(humany, ballx, bally) {
+		//var data_xs = [get_max_x() - ballx, bally];
+		var data_xs = [ballx, bally];
 		if(this.previous_data == null) {
 			this.last_data_object = [data_xs];
 			this.previous_data = data_xs;
@@ -47,9 +49,13 @@ class ai {
 		}
 		this.last_data_object = [...this.previous_data, ...data_xs];
 		this.training_data[0].push(this.last_data_object);
-		if(this.previous_data[0] > humany) this.training_data[1].push([0.0, 0.0, 1.0]);
-		else if(this.previous_data[0] < humany) this.training_data[1].push([1.0, 0.0, 0.0]);
-		else this.training_data[1].push([0.0, 1.0, 0.0]);
+		//if(this.previous_data[1] > humany) this.training_data[1].push([0.0, 0.0, 1.0]);
+		//else if(this.previous_data[1] < humany) this.training_data[1].push([1.0, 0.0, 0.0]);
+		//else this.training_data[1].push([0.0, 1.0, 0.0]);
+		var ta = new Array(get_max_y());
+		ta.fill(0.0);
+		ta[round([humany])] = 1.0;
+		this.training_data[1].push(ta);
 		this.previous_data = data_xs;
 	}
 	reset() {
@@ -74,12 +80,14 @@ class ai {
 	}
 	pred() {
 		//console.log("Predicting");
-		if(this.last_data_object != null && this.last_data_object.length == 8) {
-			//use this.last_data_object for input data
-			//do prediction here
-			//return -1/0/1
-			var pred = this.model.predict(tf.tensor([this.last_data_object]));
-			return tf.argMax(pred, 1).dataSync() - 1;
+		if(this.last_data_object != null && this.last_data_object.length == 4) {
+			var t = new Array(4);
+			t[0] = get_max_x() - this.last_data_object[0];
+			t[2] = get_max_x() - this.last_data_object[2];
+			t[1] = this.last_data_object[1];
+			t[3] = this.last_data_object[3];
+			var pred = this.model.predict(tf.tensor([t]));
+			return tf.argMax(pred, 1).dataSync();
 		}
 	}
 }
@@ -294,7 +302,7 @@ class pong {
 		var ty = me.ball["p"][1] + me.ball["v"][1];
 		var vt = Math.sqrt(me.ball["v"][0]*me.ball["v"][0] + me.ball["v"][1]*me.ball["v"][1]);
 		var crashdir = 5;
-		ai1.save_data(rk2.y, rk1.y, me.ball["p"][0], me.ball["p"][1]);
+		ai1.save_data(rk2.y, me.ball["p"][0], me.ball["p"][1]);
 		if(round(tx) > get_max_x()) {
 			tx = 2.0 * get_max_x() - tx;
 			me.ball["v"][0] *= -1;
@@ -374,14 +382,8 @@ class pong {
 		rk1.vy *= 0.8;
 		rk2.vy *= 0.8;
 		var airk = ai1.pred();
-		switch(airk) {
-			case -1:
-				rk1.incr();
-				break;
-			case +1:
-				rk1.decr();
-				break;
-		}
+		if(airk > rk1.y) rk1.incr();
+		else if(airk < rk1.y) rk1.decr();
 		return -1;
 	}
 }
